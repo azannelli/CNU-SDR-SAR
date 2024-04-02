bba00d7b9dd6f393d9340c3067029adeb2fafffe
azannelli <alex.zannelli@gmail.com>
2024-04-02 11:37:27 -0400

    small edits to vars to enable easier changing of global vars

--- Control Code/master.py
+++ Control Code/master.py
@@ -5,9 +5,11 @@ import subprocess #allows for running of GNU Radio flowgraphs
 import time #allows for time.sleep() function
 
 #**GLOBAL VARIABLES**
+#file path will need to be changed
 #signal duration will need to be adjusted based on testing
 #grc_or_python will need to be adjusted based on the file being used
 #arduino_start_key shouldnt need to be changed if using arduino code provided
+file_path = 'placeholder.grc'
 arduino_start_key = 1
 signal_duration_seconds = 10
 grc_or_python = False #true for python, false for grc
@@ -50,13 +52,13 @@ if __name__ == "__main__":
     arduino.write(bytes(arduino_start_key, 'utf-8'))
 
     #make sure to update below with the path to the flowchart
-    process = execute_gnuradio_flowchart('radar_receiver.grc', is_python_file=False)
+    process = execute_gnuradio_flowchart(file_path, is_python_file=grc_or_python)
 
     #length of signal duration will prob need to be calculated through testing
     time.sleep(signal_duration_seconds)
 
     #close gnu radio flowgraph
     process.terminate()
-    
+
     #close arduino and stop movement
     arduino.close()
