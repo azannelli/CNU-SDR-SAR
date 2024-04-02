ad6229d8949cc025b45903dd9ce1052fc2d0f388
azannelli <alex.zannelli@gmail.com>
2024-04-02 10:25:12 -0400

    added imports to master.py

--- Control Code/master.py
+++ Control Code/master.py
@@ -1,1 +1,4 @@
-print("hello world")
+#IMPORTS
+import serial #allows for serial communication to arduino
+import subprocess #allows for running of GNU Radio flowgraphs
+import time #allows for time.sleep() function
